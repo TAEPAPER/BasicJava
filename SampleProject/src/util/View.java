bd86fe5d4dbe2bdd01830776866ab334de4a6740
TaePaper <taejeong055@gmail.com>
2022-03-15 21:26:35 +0900

    commit;

---
+++ SampleProject/src/util/View.java
@@ -0,0 +1,25 @@
+package util;
+
+public interface View {
+
+	int HOME = 0;
+	int LOGIN = 1;
+	int JOIN = 2;
+	int BOARD_LIST = 3;
+	int BOARD_INSERT = 4;
+	int BOARD_READ = 5;
+	int BOARD_UPDATE = 6;
+	int BOARD_DELETE = 7;
+	
+	
+	int MUSIC_SEARCH =8;
+	int MUSIC_CHART =9;
+	int MUSIC_RECENTCHART= 10;
+	int MUSIC_RECOMCHART =11;
+	int MUSIC_MYCHART =12;
+	
+	
+	
+	
+	
+}
